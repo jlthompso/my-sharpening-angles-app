17ba1e00edb5288092f2ac1580be50d9440e5507
Joe Thompson <jthompso@mtu.edu>
2023-10-15 16:42:59 -0700

    Get photo from camera.

--- lib/src/sample_feature/create_item_view.dart
+++ lib/src/sample_feature/create_item_view.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 /// Displays detailed information about a SampleItem.
-class CreateItemView extends StatelessWidget {
+class CreateItemView extends StatefulWidget {
   const CreateItemView({super.key});
 
   static const routeName = '/new';
+
+  @override
+  State<CreateItemView> createState() => _CreateItemViewState();
+}
+
+class _CreateItemViewState extends State<CreateItemView> {
+  final _formKey = GlobalKey<FormState>();
+  final ImagePicker _picker = ImagePicker();
+
+  XFile? _photo;
 
   @override
   Widget build(BuildContext context) {
@@ -12,8 +24,40 @@ class CreateItemView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('New Entry'),
       ),
-      body: const Center(
-        child: Text('More Information Here'),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            SizedBox.square(
+                dimension: MediaQuery.of(context).size.width * 0.75,
+                child: _photo != null
+                    ? Image.file(File(_photo!.path))
+                    : IconButton(
+                        onPressed: () async {
+                          final XFile? img = await _picker.pickImage(
+                              source: ImageSource.camera);
+                          if (img != null) {
+                            setState(() {
+                              _photo = img;
+                            });
+                          }
+                        },
+                        icon: const Icon(Icons.add_a_photo_outlined),
+                      )),
+            TextFormField(
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Tool Description',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
